b4359d54b5633922cf6429c5207adfe7b57729c5
Daniel Mendonça <dmsantos@uolinc.com>
2021-03-19 08:43:09 -0300

    UPDATE: Backup name

--- scripts/genkey.sh
+++ scripts/genkey.sh
@@ -2,6 +2,6 @@
 
 docker exec -it gpg-running gpg --full-generate-key
 
-docker exec -it gpg-running gpg --output /output/$1.pgp --armor --export-secret-keys --export-options export-backup
+docker exec -it gpg-running gpg --output /output/$1.bk.gpg --armor --export-secret-keys --export-options export-backup
 
-mv $1.pgp gpg_keys/
+mv $1.bk.gpg gpg_keys/
